95ef18ca8eb27c30c152b35ee4fbbb858e4a470e
Rupesh Basnet <rupeshbasnet93@gmail.com>
2017-12-21 23:18:51 -0500

    Restructuring the templates and the project with app

--- rails/models.py
+++ rails/models.py
@@ -6,6 +6,9 @@ class FareTypes(models.Model):
     fare_name = models.CharField(max_length=20, blank=True, null=True)
     rate = models.DecimalField(max_digits=3, decimal_places=2, blank=True, null=True)
 
+    def __str__(self):
+    	return self.fare_name
+
     class Meta:
         managed = False
         db_table = 'fare_types'
@@ -20,6 +23,9 @@ class Passengers(models.Model):
     preferred_card_number = models.CharField(max_length=16, blank=True, null=True)
     preferred_billing_address = models.CharField(max_length=100, blank=True, null=True)
 
+    def __str__(self):
+    	return self.fname + " " + self.lname
+
     class Meta:
         managed = False
         db_table = 'passengers'
@@ -28,9 +34,12 @@ class Passengers(models.Model):
 class Reservations(models.Model):
     reservation_id = models.AutoField(primary_key=True)
     reservation_date = models.DateTimeField(blank=True, null=True)
-    paying_passenger = models.ForeignKey(Passengers, models.DO_NOTHING)
+    paying_passenger = models.ForeignKey(Passengers, on_delete=models.DO_NOTHING)
     card_number = models.CharField(max_length=16, blank=True, null=True)
     billing_address = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+    	return self.reservation_id
 
     class Meta:
         managed = False
@@ -38,10 +47,13 @@ class Reservations(models.Model):
 
 
 class SeatsFree(models.Model):
-    train = models.ForeignKey('Trains', models.DO_NOTHING, primary_key=True)
-    segment = models.ForeignKey('Segments', models.DO_NOTHING)
+    train = models.ForeignKey('Trains', on_delete=models.DO_NOTHING, primary_key=True)
+    segment = models.ForeignKey('Segments', on_delete=models.DO_NOTHING)
     seat_free_date = models.DateField()
     freeseat = models.IntegerField()
+
+    def __str__(self):
+    	return self.seat_free_date
 
     class Meta:
         managed = False
@@ -51,9 +63,12 @@ class SeatsFree(models.Model):
 
 class Segments(models.Model):
     segment_id = models.AutoField(primary_key=True)
-    seg_n_end = models.ForeignKey('Stations', models.DO_NOTHING, related_name='seg_n_end')
-    seg_s_end = models.ForeignKey('Stations', models.DO_NOTHING, related_name='seg_s_end')
+    seg_n_end = models.ForeignKey('Stations', on_delete=models.DO_NOTHING, related_name='seg_n_end')
+    seg_s_end = models.ForeignKey('Stations', on_delete=models.DO_NOTHING, related_name='seg_s_end')
     seg_fare = models.DecimalField(max_digits=7, decimal_places=2)
+
+    def __str__(self):
+    	return self.segment_id
 
     class Meta:
         managed = False
@@ -65,16 +80,22 @@ class Stations(models.Model):
     station_name = models.CharField(max_length=40)
     station_symbol = models.CharField(unique=True, max_length=3)
 
+    def __str__(self):
+    	return self.station_name
+
     class Meta:
         managed = False
         db_table = 'stations'
 
 
 class StopsAt(models.Model):
-    train = models.ForeignKey('Trains', models.DO_NOTHING, primary_key=True)
-    station = models.ForeignKey(Stations, models.DO_NOTHING)
+    train = models.ForeignKey('Trains', on_delete=models.DO_NOTHING, primary_key=True)
+    station = models.ForeignKey(Stations, on_delete=models.DO_NOTHING)
     time_in = models.TimeField(blank=True, null=True)
     time_out = models.TimeField(blank=True, null=True)
+
+    def __str__(self):
+    	return self.time_in + " " + self.time_out 
 
     class Meta:
         managed = False
@@ -84,10 +105,13 @@ class StopsAt(models.Model):
 
 class Trains(models.Model):
     train_id = models.AutoField(primary_key=True)
-    train_start = models.ForeignKey(Stations, models.DO_NOTHING, related_name='train_start')
-    train_end = models.ForeignKey(Stations, models.DO_NOTHING, related_name='train_end')
+    train_start = models.ForeignKey(Stations, on_delete=models.DO_NOTHING, related_name='train_start')
+    train_end = models.ForeignKey(Stations, on_delete=models.DO_NOTHING, related_name='train_end')
     train_direction = models.IntegerField(blank=True, null=True)
     train_days = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+    	return self.train_id
 
     class Meta:
         managed = False
@@ -97,13 +121,16 @@ class Trains(models.Model):
 class Trips(models.Model):
     trip_id = models.AutoField(primary_key=True)
     trip_date = models.DateField()
-    trip_seg_start = models.ForeignKey(Segments, models.DO_NOTHING, related_name='trip_seg_start')
-    trip_seg_ends = models.ForeignKey(Segments, models.DO_NOTHING, related_name='trip_seg_ends')
-    fare_type = models.ForeignKey(FareTypes, models.DO_NOTHING, related_name='fare_type')
+    trip_seg_start = models.ForeignKey(Segments, on_delete=models.DO_NOTHING, related_name='trip_seg_start')
+    trip_seg_ends = models.ForeignKey(Segments, on_delete=models.DO_NOTHING, related_name='trip_seg_ends')
+    fare_type = models.ForeignKey(FareTypes, on_delete=models.DO_NOTHING, related_name='fare_type')
     fare = models.DecimalField(max_digits=7, decimal_places=2)
-    trip_train = models.ForeignKey(Trains, models.DO_NOTHING)
-    reservation = models.ForeignKey(Reservations, models.DO_NOTHING)
+    trip_train = models.ForeignKey(Trains, on_delete=models.DO_NOTHING)
+    reservation = models.ForeignKey(Reservations, on_delete=models.DO_NOTHING)
 
+    def __str__(self):
+    	return self.trip_id
+    	
     class Meta:
         managed = False
         db_table = 'trips'
--- rails/views.py
+++ rails/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect, HttpResponse
 
 # Create your views here.
+def index(request):
+    return render(request, 'main/index.html')
