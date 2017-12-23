db6f6bf0629522b46a8de220ac11f9c1b582e93a
RAFAEL LI CHEN <rafaellichen@gmail.com>
2017-12-23 06:01:20 -0500

    able to get ticket

--- rails/urls.py
+++ rails/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^search/$', views.get_avail_trains, name='search')    
+    url(r'^search/$', views.get_avail_trains, name='search'),
+    url(r'^make-reservations/$', views.make_reservation, name="reserve"),
+    url(r'^book_ticket/$', views.book_ticket, name="book")
 ]
--- rails/views.py
+++ rails/views.py
@@ -4,10 +4,10 @@ from django.db import connection
 
 # Create your views here.
 def index(request):
-
     return render(request, 'main/index.html')
 
 def get_avail_trains(request):
+    row=""
     if request.method == 'POST':
         fromStation = request.POST.get("stationFrom")
         toStation = request.POST.get("stationTo")
@@ -29,15 +29,64 @@ def get_avail_trains(request):
         temp["train_id"] = e[0]
         temp["start_station"] = e[1]
         temp["end_station"] = e[2]
-        temp["travel_date"] = e[3]
+        temp["travel_date"] = str(e[3])
         temp["time_of_day"] = e[4]
         temp["seats_free"] = e[5]
         temp["chosen"] = e[6]
         data.append(temp)
     return render(request, 'main/index.html', {"avail_trains":data})
 
+def make_reservation(request):
+    t_id=""
+    s_station=""
+    e_station=""
+    t_date=""
+    tod=""
+    s_free=""
+    cho=""
+    if request.method == 'POST':
+        t_id = request.POST.get("ti")
+        s_station = request.POST.get("ss")
+        e_station = request.POST.get("es")
+        t_date = request.POST.get("trd")
+        tod = request.POST.get("tod")
+        s_free = request.POST.get("sf")
+        cho = request.POST.get("cho")
+    data = {}
+    data["train_id"] = t_id
+    data["start_station"] = s_station
+    data["end_station"] = e_station
+    data["travel_date"] = t_date
+    data["time_of_day"] = tod
+    data["seats_free"] = s_free
+    data["chosen"] = cho
+    return render(request, 'main/reservation.html', {"selected":data})
+
 def book_ticket(request):
-    pass
+    last_name=""
+    first_name=""
+    payment=""
+    t_id=""
+    s_station=""
+    e_station=""
+    t_date=""
+    tod=""
+    s_free=""
+    cho=""
+    if request.method=="POST":
+        last_name = request.POST.get("lname")
+        first_name = request.POST.get("fname")
+        payment = request.POST.get("pay")
+        t_id = request.POST.get("ti")
+        s_station = request.POST.get("ss")
+        e_station = request.POST.get("es")
+        t_date = request.POST.get("trd")
+    with connection.cursor() as cursor:
+            # get_ticket('Rafael','Li Chen','Free Ticket', @out_value)
+            cursor.execute("update avail_trains set chosen=1 where train_id="+t_id+" and start_station="+s_station+" and end_station="+e_station+" and travel_date='"+str(t_date)+"'")
+            ret = cursor.callproc("get_ticket", [first_name, last_name, payment, ""])
+            # row = cursor.fetchall()
+    return render(request, 'main/reservation.html', {"result":"good to go"})
 
 def my_custom_sql():
     with connection.cursor() as cursor:
