100dcf258e38e337f499526ec66e8f19f160d0e9
Rupesh Basnet <rupeshbasnet93@gmail.com>
2017-12-23 00:14:50 -0500

    Added new way to getting mysql data

--- rails/admin.py
+++ rails/admin.py
@@ -1,14 +1,2 @@
 from django.contrib import admin
 
-# Register your models here.
-from rails.models import FareTypes, Passengers, Reservations, SeatsFree, Segments, Stations, StopsAt, Trains, Trips
-
-admin.site.register(FareTypes)
-admin.site.register(Passengers)
-admin.site.register(Reservations)
-admin.site.register(SeatsFree)
-admin.site.register(Segments)
-admin.site.register(Stations)
-admin.site.register(StopsAt)
-admin.site.register(Trains)
-admin.site.register(Trips)
--- rails/views.py
+++ rails/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect, HttpResponse
+from django.db import connection
 
 # Create your views here.
 def index(request):
+    get_avail_trains()
     return render(request, 'main/index.html')
+
+def get_avail_trains():
+    out_arg1 = ""
+    with connection.cursor() as cursor:
+        ret = cursor.callproc("get_avail_trains", ['2017-12-21', 'BOS', 'WAS','AFT', 'W', out_arg1])# calls PROCEDURE named LOG_MESSAGE which resides in MY_UTIL Package
+        print (out_arg1)
+        print (ret)
+
+def my_custom_sql():
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT station_name from stations")
+        row = cursor.fetchall()
+    print (row)
+    return row
